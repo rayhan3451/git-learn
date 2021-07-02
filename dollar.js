da608ba4f1582363859eb6ef077ac5b371453779
Mohammad Rayhan <49207339+rayhan3451@users.noreply.github.com>
2021-07-02 15:23:01 +0600

    js file add

--- dollar.js
+++ dollar.js
@@ -1,3 +1,6 @@
 console.log("hello bangladesh");
 console.log("Kemon aso");
 console.log("oooooo ganja ganjaaaaaa");
+
+console.log("ki kemon aso chotto bondhu");
+console.log("valorant khelbi kina bol");
